bf3b4e1fc54abaf72fa503c12f8f3acaa70ee604
steve <scherewaty@gmail.com>
2024-03-27 10:04:05 -0500

    class02.sh

--- class02.sh
+++ class02.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
+# 03/26/2024
+
 # Script: Ops 301 Challenge 02
 # Purpose: Append date and time
 
+# Current date 
+month= date +%m
+day= date +%d
+year= date +%y
+# Current time
+hour= date +%H
+minutes= date +%M
+seconds= date +%S
+
 # Copy the syslog file to the current directory
-cp /var/log/syslog ./
+cp /var/log/syslog ./ 
+
+echo "Current Date: $day-$month-$year" >> syslog
+echo "Current Time: $hour:$minutes:$seconds" >> syslog 
 
 
