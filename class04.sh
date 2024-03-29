26f83bf4c56efea918f670a26c7478c891708a2e
steve <scherewaty@gmail.com>
2024-03-29 10:04:34 -0500

    class04.sh

--- class04.sh
+++ class04.sh
@@ -6,3 +6,29 @@
 # Author: Steve Cherewaty
 # Purpose: Conditional statement practice
 
+# echo $target
+target=$RANDOM
+let "target %= 100"
+let "target += 1"
+
+
+# create the loop
+while true; do 
+    echo "Guess a number between 1 and 100:"
+    read guess
+    echo $guess
+
+# Is the guess correct?
+    if [ $guess -eq $target ]; then 
+    echo "You got it!"
+    fi
+
+# Is the guess too high or too low?
+    if [ $guess -gt $target ]; then 
+        echo "Too high...try again."
+    else 
+        echo "Too low...try again."
+    fi
+
+done
+
