5fe65ab8aa13febd69dcc301dfc09eafe955e867
steve <scherewaty@gmail.com>
2024-04-08 12:05:00 -0500

    class11.py

---
+++ class11.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python 3
+
+# 04/08/2024
+
+# Script: Ops 301 Challenge 07
+# Author: Steve Cherewaty
+# Purpose: Psutil
+
+import psutil
+
+
+# CPU time
+print(f"CPU Time: {psutil.cpu_times()}\n")
+print(f"CPU Consumption: {psutil.cpu_percent()}\n")
+print(f"CPU Consumption: {psutil.cpu_percent()}\n")
+
