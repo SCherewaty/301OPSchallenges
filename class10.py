896ea563c72f79d39af219a139fc776de53692ac
steve <scherewaty@gmail.com>
2024-04-08 11:54:00 -0500

    class10.py

--- class10.py
+++ class10.py
@@ -11,21 +11,38 @@ import os
 if os.path.exists("testfile1.txt"):
     print("It's real!")
 
-# Open a file for reading
-file = open("testfile1.txt", "r")
+# Create new txt file
+new_file = open("testfile2.txt", "w")
+new_file.write("Here's the new test file.")
+new_file.close()
 
-file.close()
+# More practice
 
-# Try a file to write
-#file = open("testfile2.txt", "w")
-
+file = open("testfile2.txt", "w")
 file.write("Hello Worldo")
-
 file.close()
 
+# Append 3 lines
+new_file = open("testfile2.txt", "a")
+new_file.close("/n")
+new_file.write("Here's the 1 appended test file.")
+new_file.write("Here's the 2 appended test file.")
+new_file.write("Here's the 3 appended test file.")
+new_file.close()
 
+# Print one line at a time
+read_file = open("Testfile1.txt", "r")
 
+for line in read_file:
+    print(line)
 
+# Print the first line
+print(read_file.readline())
+
+# delete testfile
+os.remove("testfile2.txt")
+# rename
+# os.remove("testfile2.txt")
 
 
 
